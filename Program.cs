eeff037bfb28b1f92c3184745d2c168237d3213a
Luke Chen Shui <chenshuiluke@gmail.com>
2020-04-05 22:49:05 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WindowsTmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Path.Combine("C:\\", "tmp");
+            if(System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.Delete(path);
+            }
+            System.IO.Directory.CreateDirectory(path);
+        }
+    }
+}
